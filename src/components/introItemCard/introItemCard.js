d849cdcf56ebf502d38b440f817904d166bb2e5a
KelliePetersen <kelliepetersen@outlook.com>
2020-06-22 10:00:00 -0600

    create introItemCard component

---
+++ src/components/introItemCard/introItemCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from '../image'
+import styles from './introItemCard.module.scss'
+
+const IntroItemCard = ({ image, title, description, info }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.image} >
+        <Image filename={image} />
+      </div>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.description}>{description}</p>
+      <p className={styles.info}>{info}</p>
+    </div>
+  )
+}
+
+export default IntroItemCard
