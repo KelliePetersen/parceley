10b1c98130a9df107b59e37e9c2975b7a894d4ca
KelliePetersen <kelliepetersen@outlook.com>
2020-06-22 10:00:00 -0600

    style intro section

--- src/sections/intro.js
+++ src/sections/intro.js
@@ -5,10 +5,10 @@ const Intro = () => {
   return (
     <section className={styles.intro} id="about">
       <div className={styles.wrapper}>
-        <div style={{width: '300px', height: '500px', backgroundColor: '#333'}}></div>
-        <div style={{width: '300px', height: '500px', backgroundColor: '#333'}}></div>
+        <div style={{width: '300px', height: '450px', backgroundColor: '#333'}}></div>
+        <div style={{width: '300px', height: '450px', backgroundColor: '#333'}}></div>
         <div className={styles.container}>
-          <h2 className={styles.heading}>Delicious food is waiting for you</h2>
+          <h2 className={styles.heading}>Delicious food <br />is waiting for you</h2>
           <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
           Maec enas pharetra sit amet nunc in pulvinar edgar.  adipiscing elit. Pharetra sit amet nunc in barlet.</p>
         </div>
