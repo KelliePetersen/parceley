ca2d82b6fa69b4ac52f686a9dae90f8f71f14d32
KelliePetersen <kelliepetersen@outlook.com>
2020-06-23 10:00:00 -0600

    create recipeMenu component

---
+++ src/components/recipeMenu/recipeMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RecipeMenu = () => {
+  return (
+    <section className={styles.recipeMenu} id="recipes">
+      <h2 className={styles.heading}>Expore our menu</h2>
+    </section>
+  )
+}
+
+export default RecipeMenu
