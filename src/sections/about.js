d8b4ff4bb7b7ac571123afb5f9adb0d34c086864
KelliePetersen <kelliepetersen@outlook.com>
2020-06-23 10:00:00 -0600

    add aboutCard to about section

--- src/sections/about.js
+++ src/sections/about.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import AboutCard from '../components/aboutCard/aboutCard'
 import styles from './about.module.scss'
 
 const About = () => {
@@ -8,9 +9,18 @@ const About = () => {
       <p className={styles.subheading}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
       <br /> Maec enas pharetra sit amet nunc.</p>
       <div className={styles.grid}>
-        <div style={{width: '300px', height: '300px', backgroundColor: '#333'}}></div>
-        <div style={{width: '300px', height: '300px', backgroundColor: '#333'}}></div>
-        <div style={{width: '300px', height: '300px', backgroundColor: '#333'}}></div>
+        <AboutCard 
+          title="Pick Meals" 
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maec enas pharetra sit amet nunc."
+        />
+        <AboutCard 
+          title="Choose Offers" 
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maec enas pharetra sit amet nunc."
+        />
+        <AboutCard 
+          title="Fast Delivery" 
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maec enas pharetra sit amet nunc."
+        />
       </div>
     </section>
   )
