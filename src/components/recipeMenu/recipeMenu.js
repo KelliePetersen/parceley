b5d1e35d95961d1744fc0d49bc3e83a61743a8c5
KelliePetersen <kelliepetersen@outlook.com>
2020-06-24 10:00:00 -0600

    add html code to recipeMenu component

--- src/components/recipeMenu/recipeMenu.js
+++ src/components/recipeMenu/recipeMenu.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import styles from './recipeMenu.module.scss'
 
 const RecipeMenu = () => {
   return (
     <section className={styles.recipeMenu} id="recipes">
       <h2 className={styles.heading}>Expore our menu</h2>
+      <div className={styles.tab}>
+        <button type="button" className={styles.tablink}>Breakfast</button>
+        <button type="button" className={styles.tablink}>Lunch</button>
+        <button type="button" className={styles.tablink}>Dinner</button>
+        <button type="button" className={styles.tablink}>Dessert</button>
+        <button type="button" className={styles.tablink}>Snacks</button>
+      </div>
+      <div className={styles.grid}>
+        <div style={{height: '300px', width: '250px', backgroundColor: '#333'}}></div>
+        <div style={{height: '300px', width: '250px', backgroundColor: '#333'}}></div>
+        <div style={{height: '300px', width: '250px', backgroundColor: '#333'}}></div>
+        <div style={{height: '300px', width: '250px', backgroundColor: '#333'}}></div>
+        <div style={{height: '300px', width: '250px', backgroundColor: '#333'}}></div>
+        <div style={{height: '300px', width: '250px', backgroundColor: '#333'}}></div>
+        <div style={{height: '300px', width: '250px', backgroundColor: '#333'}}></div>
+        <div style={{height: '300px', width: '250px', backgroundColor: '#333'}}></div>
+      </div>
     </section>
   )
 }
