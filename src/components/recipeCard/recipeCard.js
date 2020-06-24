409652b8a2b6d05d359973d53bf207d019163e63
KelliePetersen <kelliepetersen@outlook.com>
2020-06-24 10:00:00 -0600

    add html code to recipeCard component

--- src/components/recipeCard/recipeCard.js
+++ src/components/recipeCard/recipeCard.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Image from '../image'
+import styles from './recipeCard.module.scss'
 
-const RecipeCard = () => {
+const RecipeCard = ({ image, title, description, info }) => {
   return (
-    <div>
-      
+    <div className={styles.card}>
+      <div className={styles.image} >
+        <Image filename={image} />
+      </div>
+      <p className={styles.title}>{title}</p>
+      <p className={styles.description}>{description}</p>
+      <p className={styles.info}>{info}</p>
     </div>
   )
 }
