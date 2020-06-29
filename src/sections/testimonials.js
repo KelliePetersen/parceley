f72cbf7f8f1fe19773fee47c8a246c57d9f297d3
KelliePetersen <kelliepetersen@outlook.com>
2020-06-29 10:00:00 -0600

    add styling to testimonials section

--- src/sections/testimonials.js
+++ src/sections/testimonials.js
@@ -4,7 +4,12 @@ import styles from './testimonials.module.scss'
 const Testimonials = () => {
   return (
     <section className={styles.testimonials} id="testimonials">
-      
+      <h2>What people are saying</h2>
+      <p className={styles.subheading}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+        Maec enas pharetra sit amet nunc.</p>
+      <div className={styles.grid}>
+
+      </div>
     </section>
   )
 }
