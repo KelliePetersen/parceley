3a0ca413416502895e4793827c0bbe3b2d55bfc2
KelliePetersen <kelliepetersen@outlook.com>
2020-06-29 10:00:00 -0600

    create testimonials section

---
+++ src/sections/testimonials.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './testimonials.module.scss'
+
+const Testimonials = () => {
+  return (
+    <section className={styles.testimonials} id="testimonials">
+      
+    </section>
+  )
+}
+
+export default Testimonials
