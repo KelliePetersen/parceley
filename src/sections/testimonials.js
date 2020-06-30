1ce68c39c5f180925bca98d8ddb203f94985b70f
KelliePetersen <kelliepetersen@outlook.com>
2020-06-30 10:00:00 -0600

    position reviewCards in testimonials section

--- src/sections/testimonials.js
+++ src/sections/testimonials.js
@@ -9,7 +9,9 @@ const Testimonials = () => {
       <p className={styles.subheading}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
         Maec enas pharetra sit amet nunc.</p>
       <div className={styles.grid}>
-      <ReviewCard name="Jane Doe" location="NY, USA" />
+      <div style={{position: 'relative', left: '-50px', top: '20px'}}>
+        <ReviewCard name="Jane Doe" location="NY, USA" />
+      </div>
       <ReviewCard 
         name="Jane Doe" 
         location="NY, USA" 
@@ -17,10 +19,16 @@ const Testimonials = () => {
         adipisc elit. Maec enas pharet
         sit amet nunc consectur."
       />
+      <div style={{position: 'relative', top: '50px', left: '40px'}}>
+        <ReviewCard name="Jane Doe" location="NY, USA" />
+      </div>
       <ReviewCard name="Jane Doe" location="NY, USA" />
-      <ReviewCard name="Jane Doe" location="NY, USA" />
-      <ReviewCard name="Jane Doe" location="NY, USA" />
-      <ReviewCard name="Jane Doe" location="NY, USA" />
+      <div style={{position: 'relative', top: '50px', left: '70px'}}>
+        <ReviewCard name="Jane Doe" location="NY, USA" />
+      </div>
+      <div style={{position: 'relative', top: '20px', left: '70px'}}>
+        <ReviewCard name="Jane Doe" location="NY, USA" />
+      </div>
       </div>
     </section>
   )
