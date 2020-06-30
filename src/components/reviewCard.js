74ea7f0ec2105105972be0c4b5fdf26986449c9d
KelliePetersen <kelliepetersen@outlook.com>
2020-06-30 10:00:00 -0600

    style reviewCard component

--- src/components/reviewCard.js
+++ src/components/reviewCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Image from './image'
+import styles from './reviewCard.module.scss'
 
 const ReviewCard = ({ image, name, location, description }) => {
   return (
-    <div>
-      <Image filename={image} />
-      <h4>{name}</h4>
-      <p>{location}</p>
-      <p>{description}</p>
+    <div className={styles.card}>
+      <div className={styles.image}><Image filename={image} /></div>
+      <h4 className={styles.name}>{name}</h4>
+      <p className={styles.location}>{location}</p>
+      <p className={styles.description}>{description}</p>
     </div>
   )
 }
