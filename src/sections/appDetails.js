773ef592855e85ece30b72a3d6f8292d7fbf7425
KelliePetersen <kelliepetersen@outlook.com>
2020-06-30 10:00:00 -0600

    add html code to appDetails component

--- src/sections/appDetails.js
+++ src/sections/appDetails.js
@@ -4,7 +4,30 @@ import styles from './appDetails.module.scss'
 const AppDetails = () => {
   return (
     <section className={styles.appDetails} id="appDetails">
-      
+      <div className={styles.wrapper}>
+        <div>
+          <div className={styles.background}></div>
+          <div className={styles.phone}></div>
+          <div className={styles.phone}></div>
+        </div>
+        <div>
+          <h2 className={styles.heading}>Delicious food is waiting for you</h2>
+          <p className={styles.subheading}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Maec enas pharetra sit amet nunc.</p>
+          <div className={styles.card}>
+            <div className={styles.icon}></div>
+            <h4 className={styles.title}>Lorem Ipsum</h4>
+            <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Maec enas pharetra sit amet nunc.</p>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.icon}></div>
+            <h4 className={styles.title}>Lorem Ipsum</h4>
+            <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Maec enas pharetra sit amet nunc.</p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
