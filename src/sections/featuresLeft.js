f80171849ee79e930ec389c8839893e6e8af1af7
KelliePetersen <kelliepetersen@outlook.com>
2020-07-01 10:00:00 -0600

    add appDescription component to FeaturesLeft section

--- src/sections/featuresLeft.js
+++ src/sections/featuresLeft.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './featuresLeft.module.scss'
+import AppDescription from '../components/appDescription'
 
 const FeaturesLeft = () => {
   return (
@@ -10,27 +11,7 @@ const FeaturesLeft = () => {
           <div className={styles.phone}></div>
           <div className={styles.phone} style={{marginLeft: '40px', position: 'relative', top: '40px'}}></div>
         </div>
-        <div>
-          <h2 className={styles.heading}>Delicious food <br />is waiting for you</h2>
-          <p className={styles.subheading}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> 
-          Maec enas pharetra sit amet nunc.</p>
-          <div className={styles.card}>
-            <div className={styles.icon}></div>
-            <div>
-              <h4 className={styles.title}>Lorem Ipsum</h4>
-              <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Maec enas pharetra sit amet nunc.</p>
-            </div>
-          </div>
-          <div className={styles.card}>
-            <div className={styles.icon}></div>
-            <div>
-              <h4 className={styles.title}>Lorem Ipsum</h4>
-              <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Maec enas pharetra sit amet nunc.</p>
-            </div>
-          </div>
-        </div>
+        <AppDescription />
       </div>
     </section>
   )
