1d10c13ee33d22230fdaccd804be9aca8014c51e
KelliePetersen <kelliepetersen@outlook.com>
2020-07-01 10:00:00 -0600

    add html code to appDescription component

--- src/components/appDescription.js
+++ src/components/appDescription.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import styles from './appDescription.module.scss'
 
 const AppDescription = () => {
   return (
     <div>
-      
+      <h2 className={styles.heading}>Delicious food <br />is waiting for you</h2>
+      <p className={styles.subheading}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> 
+      Maec enas pharetra sit amet nunc.</p>
+      <div className={styles.card}>
+        <div className={styles.icon}></div>
+        <div>
+          <h4 className={styles.title}>Lorem Ipsum</h4>
+          <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Maec enas pharetra sit amet nunc.</p>
+        </div>
+      </div>
+      <div className={styles.card}>
+        <div className={styles.icon}></div>
+        <div>
+          <h4 className={styles.title}>Lorem Ipsum</h4>
+          <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Maec enas pharetra sit amet nunc.</p>
+        </div>
+      </div>
     </div>
   )
 }
