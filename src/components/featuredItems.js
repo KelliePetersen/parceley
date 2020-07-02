282ed1839d3a887c4e0503c4915965eb7cbdedf2
KelliePetersen <kelliepetersen@outlook.com>
2020-07-02 10:00:00 -0600

    add html code for featuredItems

--- src/components/featuredItems.js
+++ src/components/featuredItems.js
@@ -2,8 +2,16 @@ import React from 'react'
 
 const FeaturedItems = () => {
   return (
-    <div>
-      
+    <div className={styles.featuredItems}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.heading}>Featured Foods</h2>
+        <div className={styles.carousel}>
+
+        </div>
+        <div className={styles.arrows}>
+          
+        </div>
+      </div>
     </div>
   )
 }
