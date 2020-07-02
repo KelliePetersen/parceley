be7e9a4763a585d7e3893d9be3faf3f050779305
KelliePetersen <kelliepetersen@outlook.com>
2020-07-02 10:00:00 -0600

    add styling for featuredItems component

--- src/components/featuredItems.js
+++ src/components/featuredItems.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import styles from './featuredItems.module.scss'
 
 const FeaturedItems = () => {
   return (
     <div className={styles.featuredItems}>
       <div className={styles.wrapper}>
-        <h2 className={styles.heading}>Featured Foods</h2>
+        <h3 className={styles.heading}>Featured Foods</h3>
         <div className={styles.carousel}>
-
+          <div style={{width: '20%', height: '150px', backgroundColor: '#333'}}></div>
+          <div style={{width: '20%', height: '150px', backgroundColor: '#333'}}></div>
+          <div style={{width: '20%', height: '150px', backgroundColor: '#333'}}></div>
+          <div style={{width: '20%', height: '150px', backgroundColor: '#333'}}></div>
         </div>
         <div className={styles.arrows}>
-          
+
         </div>
       </div>
     </div>
