38f8ff7349024058077af0f32b63093c37c980af
KelliePetersen <kelliepetersen@outlook.com>
2020-07-03 10:00:00 -0600

    add html structure to featuredItemCard

--- src/components/featuredItemCard.js
+++ src/components/featuredItemCard.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Image from './image'
+import styles from '../styles/featuredItemCard.module.scss'
 
-const FeaturedItemCard = () => {
+const FeaturedItemCard = ({ image, title, info }) => {
   return (
-    <div>
-      
+    <div className={styles.card}>
+      <Image filename={image} />
+      <p className={styles.title}>{title}</p>
+      <p className={styles.info}>{info}</p>
     </div>
   )
 }
