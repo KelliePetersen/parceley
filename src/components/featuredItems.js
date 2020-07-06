634544365f231bfd848df173cf19c08f620eb90c
KelliePetersen <kelliepetersen@outlook.com>
2020-07-06 10:00:00 -0600

    add arrows to featuredItems

--- src/components/featuredItems.js
+++ src/components/featuredItems.js
@@ -14,7 +14,8 @@ const FeaturedItems = () => {
           <FeaturedItemCard image="bowl-small.png" title='Grilled Chicken Salad' info='980kj' />
         </div>
         <div className={styles.arrows}>
-
+          <button type="button" className={`${styles.arrow} ${styles.arrowLeft}`}>&larr;</button>
+          <button type="button" className={`${styles.arrow} ${styles.arrowRight}`}>&rarr;</button>
         </div>
       </div>
     </div>
