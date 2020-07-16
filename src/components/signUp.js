128dc3c3c559e08fe807f862c05b752a5ec32093
KelliePetersen <kelliepetersen@outlook.com>
2020-07-16 10:00:00 -0600

    change label for to htmlFor

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -4,7 +4,7 @@ import styles from './signUp.module.scss'
 const SignUp = () => {
   return (
     <form className={styles.form}>
-      <label for="email" style={{position: 'relative'}}>
+      <label htmlFor="email" style={{position: 'relative'}}>
         <span className={styles.label}><div className={styles.icon}></div>Enter your email</span>
         <input type="email" name="email" id="email" className={styles.email} />
       </label>
