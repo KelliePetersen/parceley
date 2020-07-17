077355521c01d264247e672dc8ca3267ad806e67
KelliePetersen <kelliepetersen@outlook.com>
2020-07-17 18:49:11 +1000

    update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Parceley | Food Delivery App`,
+    description: `A meal, ingredient & recipe delivery app for favored meals from approved restaurants.`,
+    author: `Kellie Petersen`,
   },
   plugins: [
     `gatsby-plugin-sass`,
