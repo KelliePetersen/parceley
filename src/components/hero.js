11b8f6d9cf98608f2393f2c0dedbb1cde1ce1285
KelliePetersen <kelliepetersen@outlook.com>
2020-07-18 16:17:29 +1000

    create empty hero component

---
+++ src/components/hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './hero.module.scss'
+
+const Hero = () => {
+  return (
+    <div className={styles.hero} id="hero">
+      
+    </div>
+  )
+}
+
+export default Hero
