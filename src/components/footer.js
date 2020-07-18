91e2d1824726f0024c04c38ce3b1075ce562f052
KelliePetersen <kelliepetersen@outlook.com>
2020-07-18 17:43:34 +1000

    create footer component containing copyright

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        © {new Date().getFullYear()} Parceley | 
+        <span>Design by Parchot</span>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,9 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from "./footer"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -28,11 +22,7 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata.title} />
       <div>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+        <Footer />
       </div>
     </>
   )
