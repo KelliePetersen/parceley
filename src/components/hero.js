df5ede8dd17bfd2be2e266c42939218f8b7e8b46
KelliePetersen <kelliepetersen@outlook.com>
2020-07-18 16:31:18 +1000

    style text content in header component

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,7 +4,17 @@ import styles from './hero.module.scss'
 const Hero = () => {
   return (
     <div className={styles.hero} id="hero">
-      
+      <div className={styles.container}>
+        <h1 className={styles.heading}>
+          Delicious food <br />is waiting for you
+        </h1>
+        <p className={styles.subheading}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maec enas pharetra sit amet nunc in pulvinar.
+        </p>
+        <div>
+          placeholder
+        </div>
+      </div>
     </div>
   )
 }
