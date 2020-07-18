660f7c7c91b8e268d6e05af60ecc261d41852d87
KelliePetersen <kelliepetersen@outlook.com>
2020-07-18 17:40:16 +1000

    add sections and styles folders

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Hero from "../components/hero"
+import Hero from "../sections/hero"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
--- src/sections/hero.js
+++ src/sections/hero.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Image from './image'
-import SignUp from './signUp'
+import Image from '../components/image'
+import SignUp from '../components/signUp'
 import styles from './hero.module.scss'
 
 const Hero = () => {
