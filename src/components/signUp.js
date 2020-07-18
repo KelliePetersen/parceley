b26d568a711653c58ba6562b946ee45d42d10670
KelliePetersen <kelliepetersen@outlook.com>
2020-07-18 17:32:26 +1000

    add email icon

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -5,7 +5,7 @@ const SignUp = () => {
   return (
     <form className={styles.form}>
       <label for="email" style={{position: 'relative'}}>
-        <span className={styles.label}>Enter your email</span>
+        <span className={styles.label}><div className={styles.icon}></div>Enter your email</span>
         <input type="email" name="email" id="email" className={styles.email} />
       </label>
       <input type="submit" value="Sign Up" className={styles.button} />
