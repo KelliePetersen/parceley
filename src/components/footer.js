d626312828f4ee94ca57ade773a67047607d7189
KelliePetersen <kelliepetersen@outlook.com>
2020-07-18 17:53:47 +1000

    style copyright in footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import styles from './footer.module.scss'
 
 const Footer = () => {
   return (
-    <footer>
-      <p>
-        © {new Date().getFullYear()} Parceley | 
-        <span>Design by Parchot</span>
+    <footer className={styles.footer}>
+      <div className={styles.divider}></div>
+      <p className={styles.copyright}>
+        © {new Date().getFullYear()} Parceley <span className={styles.line}>|</span> 
+        <span className={styles.attribution}>Design by <a href="https://www.parchot.com" target="_blank">Parchot</a></span>
       </p>
     </footer>
   )
