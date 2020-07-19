76398ff1dd124725bc116087e77f124814246341
KelliePetersen <kelliepetersen@outlook.com>
2020-07-19 10:00:00 -0600

    move the image in features section

--- src/sections/features.js
+++ src/sections/features.js
@@ -6,10 +6,10 @@ import styles from './features.module.scss'
 const Features = () => {
   return (
     <section className={styles.features} id="features">
-      <div className={styles.wrapper}>
+      <div className={styles.wrapper} style={{marginLeft: 'auto', marginRight: 'auto', columnGap: '0'}}>
         <AppDescription />
         <div style={{position: 'relative'}}>
-          <div className={styles.background}></div>
+          <div className={styles.background} style={{left: 'auto'}}></div>
           <Image filename="bowl3.png" styling={{width: '550px'}} />
         </div>
       </div>
