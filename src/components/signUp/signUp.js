72f949ef7d2fb491ab0f382476a6b8dea63a8906
KelliePetersen <kelliepetersen@outlook.com>
2020-07-20 10:00:00 -0600

    make signUp component responsive

--- src/components/signUp/signUp.js
+++ src/components/signUp/signUp.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './signUp.module.scss'
 
 const SignUp = () => {
+  const [buttonValue, setButtonValue] = useState('→')
+
+  useEffect(() => {
+    let windowState = () => (window.innerWidth > 767) ? setButtonValue('Sign Up') : setButtonValue('→')
+    windowState();
+    window.addEventListener('resize', windowState)
+  }, [])
+
   return (
     <form className={styles.form}>
       <label htmlFor="email" style={{position: 'relative'}}>
         <span className={styles.label}><div className={styles.icon}></div>Enter your email</span>
         <input type="email" name="email" id="email" className={styles.email} />
       </label>
-      <input type="submit" value="Sign Up" className={styles.button} />
+      <input type="submit" value={buttonValue} className={styles.button} />
     </form>
   )
 }
