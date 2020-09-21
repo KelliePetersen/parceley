72fa597ef39f4b20a6b1d35ae1107e925ccdfc4b
KelliePetersen <kelliepetersen@outlook.com>
2020-09-21 12:24:02 -0600

    add new logo

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Link } from "gatsby"
+import Image from '../image'
 import styles from './logo.module.scss'
 
 const Logo = ({ styling }) => {
   return (
-    <Link className={styles.logo} to="/#" style={styling}>parceley</Link>
+    <Link className={styles.logo} to="/#" style={styling}>
+      <div className={styles.image}>
+        <Image filename="logo.png" />
+      </div>
+      parceley
+    </Link>
   )
 }
 
