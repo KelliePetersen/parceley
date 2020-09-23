4faf679dca011b83a197f9696639d0cdb996f439
KelliePetersen <kelliepetersen@outlook.com>
2020-09-23 10:17:25 -0600

    change how elements are positioned and grouped in hero

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -8,20 +8,23 @@ const Hero = () => {
   return (
     <div className={styles.hero} id="hero">
       <div className={styles.container}>
-        <h1 className={styles.heading}>
-          Delicious food <br />is waiting for you
-        </h1>
-        <p className={styles.subheading}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
-          Maec enas pharetra sit amet nunc in pulvinar.
-        </p>
-        <SignUp />
-      </div>
-      <div className={styles.background}>
-        <div className={styles.image}>
-          <Image filename="bowl3.png" />
+        <div className={styles.textbox}>
+          <h1 className={styles.heading}>
+            Delicious food <br />is waiting for you
+          </h1>
+          <p className={styles.subheading}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
+            Maec enas pharetra sit amet nunc in pulvinar.
+          </p>
+          <SignUp />
+        </div>
+        <div className={styles.imageContainer}>
+          <div className={styles.image}>
+            <Image filename="hero.png" />
+          </div>
         </div>
       </div>
+      <div className={styles.background} />
       <SocialMedia styling={{position: 'absolute', bottom: '50px', right: '50px'}} />
     </div>
   )
