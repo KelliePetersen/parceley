10c3c241af423845b2697482732436094ae03ca3
KelliePetersen <kelliepetersen@outlook.com>
2020-09-24 11:33:21 -0600

    style featuredItems section and add text

--- src/components/featuredItems/featuredItems.js
+++ src/components/featuredItems/featuredItems.js
@@ -5,17 +5,19 @@ import FeaturedItemCard from '../featuredItemCard/featuredItemCard'
 const FeaturedItems = () => {
   return (
     <div className={styles.featuredItems}>
-      <div className={styles.wrapper}>
-        <h3 className={styles.heading}>Featured Foods</h3>
-        <div className={styles.carousel}>
-          <FeaturedItemCard image="featured1.png" title='Grilled Chicken Salad' info='980kj' />
-          <FeaturedItemCard image="featured2.png" title='Grilled Chicken Salad' info='980kj' imageSize='175px' />
-          <FeaturedItemCard image="featured3.png" title='Grilled Chicken Salad' info='980kj' />
-          <FeaturedItemCard image="featured4.png" title='Grilled Chicken Salad' info='980kj' imageSize='150px' />
-        </div>
-        <div className={styles.arrows}>
-          <button type="button" className={`${styles.arrow} ${styles.arrowLeft}`}>&larr;</button>
-          <button type="button" className={`${styles.arrow} ${styles.arrowRight}`}>&rarr;</button>
+      <div className={styles.background}>
+        <div className={styles.wrapper}>
+          <h3 className={styles.heading}>Featured Foods</h3>
+          <div className={styles.carousel}>
+            <FeaturedItemCard image="featured1.png" title='Mediterranean Salad' info='1040kj' />
+            <FeaturedItemCard image="featured2.png" title='Grilled Chicken Salad' info='980kj' imageSize='175px' />
+            <FeaturedItemCard image="featured3.png" title='French Eggs Benedict' info='1120kj' />
+            <FeaturedItemCard image="featured4.png" title='Chicken Tomato Mix' info='980kj' imageSize='1230kj' />
+          </div>
+          <div className={styles.arrows}>
+            <button type="button" className={`${styles.arrow} ${styles.arrowLeft}`}>&larr;</button>
+            <button type="button" className={`${styles.arrow} ${styles.arrowRight}`}>&rarr;</button>
+          </div>
         </div>
       </div>
     </div>
