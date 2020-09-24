f0b9648f64b90c8c47f540695ed9b858dd36095e
KelliePetersen <kelliepetersen@outlook.com>
2020-09-24 11:28:21 -0600

    style featuredItemCards and add custom sizing

--- src/components/featuredItemCard/featuredItemCard.js
+++ src/components/featuredItemCard/featuredItemCard.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Image from '../image'
 import styles from './featuredItemCard.module.scss'
 
-const FeaturedItemCard = ({ image, title, info }) => {
+const FeaturedItemCard = ({ image, title, info, imageSize }) => {
   return (
     <div className={styles.card}>
-      <div className={styles.image} >
+      <div className={styles.image} style={{width: imageSize, height: imageSize, left: `calc(-${imageSize} / 2)`}} >
         <Image filename={image} />
       </div>
       <p className={styles.title}>{title}</p>
--- src/components/featuredItems/featuredItems.js
+++ src/components/featuredItems/featuredItems.js
@@ -8,10 +8,10 @@ const FeaturedItems = () => {
       <div className={styles.wrapper}>
         <h3 className={styles.heading}>Featured Foods</h3>
         <div className={styles.carousel}>
-          <FeaturedItemCard image="bowl-small.png" title='Grilled Chicken Salad' info='980kj' />
-          <FeaturedItemCard image="bowl-small.png" title='Grilled Chicken Salad' info='980kj' />
-          <FeaturedItemCard image="bowl-small.png" title='Grilled Chicken Salad' info='980kj' />
-          <FeaturedItemCard image="bowl-small.png" title='Grilled Chicken Salad' info='980kj' />
+          <FeaturedItemCard image="featured1.png" title='Grilled Chicken Salad' info='980kj' />
+          <FeaturedItemCard image="featured2.png" title='Grilled Chicken Salad' info='980kj' imageSize='175px' />
+          <FeaturedItemCard image="featured3.png" title='Grilled Chicken Salad' info='980kj' />
+          <FeaturedItemCard image="featured4.png" title='Grilled Chicken Salad' info='980kj' imageSize='150px' />
         </div>
         <div className={styles.arrows}>
           <button type="button" className={`${styles.arrow} ${styles.arrowLeft}`}>&larr;</button>
