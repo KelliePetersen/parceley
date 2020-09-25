4446f56de830dd75dbebc0ba77c6d31dcfd9d1e4
KelliePetersen <kelliepetersen@outlook.com>
2020-09-25 11:42:18 -0600

    add accents to features section

--- src/sections/features.js
+++ src/sections/features.js
@@ -8,6 +8,8 @@ const Features = () => {
     <section className={styles.features} id="features">
         <AppDescription />
         <div className={styles.image}>
+          <div className={styles.nuts}><Image filename="nuts.png" /></div>
+          <div className={styles.parsley}><Image filename="parsley.png" /></div>
           <Image filename="large.png" />
         </div>
       <div className={styles.background}></div>
