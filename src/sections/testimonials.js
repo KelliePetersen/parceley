da3c91a99d0cd38aacae77871f2bea414985afc7
KelliePetersen <kelliepetersen@outlook.com>
2020-09-25 10:57:23 -0600

    style testimonials section

--- src/sections/testimonials.js
+++ src/sections/testimonials.js
@@ -10,24 +10,24 @@ const Testimonials = () => {
         Maec enas pharetra sit amet nunc.</p>
       <div className={styles.grid}>
       <div style={{position: 'relative', left: '-50px', top: '20px'}}>
-        <ReviewCard name="Jane Doe" location="NY, USA" />
+        <ReviewCard image="person2.jpg" name="Jane Doe" location="NY, USA" />
       </div>
       <ReviewCard 
-        name="Jane Doe" 
+        image="person1.jpg" name="Jane Doe" 
         location="NY, USA" 
         description="Lorem ipsum dolor sit amet, con
         adipisc elit. Maec enas pharet
         sit amet nunc consectur."
       />
       <div style={{position: 'relative', top: '50px', left: '40px'}}>
-        <ReviewCard name="Jane Doe" location="NY, USA" />
+        <ReviewCard image="person3.jpg" name="Jane Doe" location="NY, USA" />
       </div>
-      <ReviewCard name="Jane Doe" location="NY, USA" />
+      <ReviewCard image="person4.jpg" name="Jane Doe" location="NY, USA" />
       <div style={{position: 'relative', top: '50px', left: '70px'}}>
-        <ReviewCard name="Jane Doe" location="NY, USA" />
+        <ReviewCard image="person5.jpg" name="Jane Doe" location="NY, USA" />
       </div>
       <div style={{position: 'relative', top: '20px', left: '70px'}}>
-        <ReviewCard name="Jane Doe" location="NY, USA" />
+        <ReviewCard image="person6.jpg" name="Jane Doe" location="NY, USA" />
       </div>
       </div>
     </section>
