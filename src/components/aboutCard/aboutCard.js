341e5b45e839eeb13c492d31fed01a191afc4e22
KelliePetersen <kelliepetersen@outlook.com>
2020-09-29 12:21:48 -0600

    add text and icons to about section

--- src/components/aboutCard/aboutCard.js
+++ src/components/aboutCard/aboutCard.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Image from '../image'
 import styles from './aboutCard.module.scss'
 
 const AboutCard = ({ image, title, description }) => {
   return (
     <div className={styles.card}>
-      <div className={styles.image} style={{backgroundImage: `url(../../images/${image})`}}></div>
+      <div className={styles.image}>
+        <div className={styles.icon}><Image filename={image} /></div>
+      </div>
       <p className={styles.title}>{title}</p>
       <p className={styles.description}>{description}</p>
     </div>
--- src/sections/about.js
+++ src/sections/about.js
@@ -10,17 +10,20 @@ const About = () => {
       <p className={styles.subheading}>Just open the app, choose your food, 
       and wait for it be delivered straight to your doorstep.</p>
       <div className={styles.grid}>
-        <AboutCard 
-          title="Pick Meals" 
+        <AboutCard
+          image="groceries.png"
+          title="Choose Meals" 
           description="Choose your meals (and dietary preferences) on the Parceley App. We handle everything from there."
         />
-        <AboutCard 
-          title="Choose Offers" 
-          description="Special offers will be available every week, giving you unique or even free food."
+        <AboutCard
+          image="truck.png"
+          title="Safe Delivery" 
+          description="Our insulated boxes are hard to damage. Safely delivered by priority express, so you never miss a meal."
         />
-        <AboutCard 
-          title="Fast Delivery" 
-          description="Our insulated boxes are delivered by priority express, so you never miss a meal."
+        <AboutCard
+          image="food.png"
+          title="Easy to Prepare" 
+          description="We make it easy. Each meal takes less than an hour to prepare &amp; cook, with enough servings to last for days."
         />
         <div className={styles.nuts}><Image filename="nuts4.png" /></div>
         <div className={styles.parsley}><Image filename="parsley2.png" /></div>
