9b56500b9c6fd172834ecd03d4bc086f6585acfe
KelliePetersen <kelliepetersen@outlook.com>
2020-09-29 10:43:12 -0600

    replace tabs with subheading

--- src/components/recipeMenu/recipeMenu.js
+++ src/components/recipeMenu/recipeMenu.js
@@ -8,13 +8,7 @@ const RecipeMenu = () => {
     <section className={styles.recipeMenu} id="recipes">
       <div className={styles.wrapper}>
         <h2 className={styles.heading}>Explore our menu</h2>
-        <div className={styles.tab}>
-          <button type="button" className={styles.tablink}>Breakfast</button>
-          <button type="button" className={styles.tablink}>Lunch</button>
-          <button type="button" className={styles.tablink}>Dinner</button>
-          <button type="button" className={styles.tablink}>Dessert</button>
-          <button type="button" className={styles.tablink} style={{marginRight: '0'}}>Snacks</button>
-        </div>
+        <p className={styles.subheading}>A small preview of the meals available to our special test members.</p>
         <div className={styles.grid}>
           <RecipeCard image="burger.jpg" title="Grilled Salad" description="Real chicken" info="980kj" />
           <RecipeCard image="burger.jpg" title="Grilled Salad" description="Real chicken" info="980kj" />
