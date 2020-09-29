9e4d71ad070c08e7e869b2b5e197ae3b9e826ff5
KelliePetersen <kelliepetersen@outlook.com>
2020-09-29 12:30:38 -0600

    add aria labels to social media icon links

--- src/components/socialMedia/socialMedia.js
+++ src/components/socialMedia/socialMedia.js
@@ -4,15 +4,15 @@ import styles from './socialMedia.module.scss'
 const SocialMedia = ({ styling }) => {
   return (
     <div className={styles.container} style={styling}>
-      <a target="_blank" rel="noreferrer" href="https://www.instagram.com/parchotsoftware" className={styles.social}
+      <a target="_blank" rel="noreferrer" href="https://www.instagram.com/parchotsoftware" className={styles.social} aria-label="instagram"
         style={{marginLeft: '0'}}
       >
         <div className={styles.instagram}></div>
       </a>
-      <a target="_blank" rel="noreferrer" href="https://www.facebook.com/parchotsoftware" className={styles.social}>
+      <a target="_blank" rel="noreferrer" href="https://www.facebook.com/parchotsoftware" className={styles.social} aria-label="facebook">
         <div className={styles.facebook}></div>
       </a>
-      <a target="_blank" rel="noreferrer" href="https://www.twitter.com/parchotsoftware" className={styles.social}>
+      <a target="_blank" rel="noreferrer" href="https://www.twitter.com/parchotsoftware" className={styles.social} aria-label="twitter">
         <div className={styles.twitter}></div>
       </a>
     </div>
