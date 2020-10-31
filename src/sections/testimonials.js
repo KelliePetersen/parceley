854b0d2d22c7c06243e5bf91fb6775837829d3b6
KelliePetersen <kelliepetersen@outlook.com>
2020-10-31 17:34:13 +1000

    add accent image to testimonials section

--- src/sections/testimonials.js
+++ src/sections/testimonials.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from '../components/image'
 import ReviewCard from '../components/reviewCard/reviewCard'
 import styles from './testimonials.module.scss'
 
@@ -12,13 +13,16 @@ const Testimonials = () => {
       <div style={{position: 'relative', left: '-50px', top: '20px'}}>
         <ReviewCard image="person2.jpg" name="Jane Doe" location="NY, USA" />
       </div>
-      <ReviewCard 
-        image="person1.jpg" name="Jane Doe" 
-        location="NY, USA" 
-        description="Lorem ipsum dolor sit amet, con
-        adipisc elit. Maec enas pharet
-        sit amet nunc consectur."
-      />
+      <div style={{position: 'relative'}}>
+        <ReviewCard 
+          image="person1.jpg" name="Jane Doe" 
+          location="NY, USA" 
+          description="Lorem ipsum dolor sit amet, con
+          adipisc elit. Maec enas pharet
+          sit amet nunc consectur."
+        />
+        <div className={styles.spinach}><Image filename="spinach.png" /></div>
+      </div>
       <div style={{position: 'relative', top: '50px', left: '40px'}}>
         <ReviewCard image="person3.jpg" name="Jane Doe" location="NY, USA" />
       </div>
