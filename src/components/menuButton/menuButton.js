e9b284dca60128789256901c39e9313524b64ff4
KelliePetersen <kelliepetersen@outlook.com>
2020-12-01 13:09:50 +1000

    add animation to menu button

--- src/components/menuButton/menuButton.js
+++ src/components/menuButton/menuButton.js
@@ -4,10 +4,10 @@ import styles from './menuButton.module.scss'
 const MenuButton = ({ styling, modalOpen, setModalOpen }) => {
 
   const handleModal = () => modalOpen ? setModalOpen(false) : setModalOpen(true)
-  
+
   return (
     <button type="button" className={styles.menu} style={styling} aria-label="open menu" onClick={handleModal}>
-      <div className={styles.line}></div>
+      <div className={`${styles.line} ${modalOpen ? styles.cross : undefined}`}></div>
     </button>
   )
 }
